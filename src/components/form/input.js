e70e20fab0263bdbce8913a5bdb06d8f44bf617c
Anantajit JG <anantajitjg2010@gmail.com>
2020-06-17 01:32:54 +0530

    ADD: File handling in form

--- src/components/form/input.js
+++ src/components/form/input.js
@@ -4,35 +4,50 @@ import { FormIdContext } from './form';
 
 const Input = ( { state, actions, inputProps } ) => {
     const jobId = useContext(FormIdContext);
-    const inputName = inputProps.name;
+    const fileInput = React.useRef();
 
+    const inputName = inputProps.name;
+    const inputType = inputProps.type;
+    
     if (typeof state.awsmjobs.forms[ jobId ].values[ inputName ] === 'undefined') {
         let initialValue = inputProps.value;
-        if ( inputProps.type === 'checkbox' ) {
+        if ( inputType === 'checkbox' ) {
             initialValue = '';
         }
         actions.awsmjobs.setFieldValue( jobId, inputName, initialValue );
     }
 
     const onChangeHandler = (e) => {
-        actions.awsmjobs.setFieldValue( jobId, inputName, e.target.value );
+        let currentValue = '';
+        if ( inputType === 'file' ) {
+            currentValue = e.target.files[0];
+        } else {
+            currentValue = e.target.value;
+        }
+        actions.awsmjobs.setFieldValue( jobId, inputName, currentValue );
     };
 
     let value = state.awsmjobs.forms[ jobId ].values[ inputName ];
-    if ( inputProps.type === 'checkbox' && value === '' ) {
+    if ( inputType === 'checkbox' && value === '' ) {
         value = inputProps.value;
     }
 
+    let componentProps = {
+        type: inputType,
+        name: inputName,
+        className: inputProps.className,
+        id: inputProps.id,
+        'aria-required': inputProps.ariaRequired,
+        onChange: onChangeHandler
+    };
+    if ( inputType === 'file' ) {
+        componentProps.ref = fileInput;
+    } else {
+        componentProps.value = value;
+    }
+
     return (
-        <input
-            type={ inputProps.type }
-            name={ inputProps.name }
-            className={ inputProps.className }
-            id={ inputProps.id }
-            value={ value }
-            aria-required={ inputProps.ariaRequired }
-            onChange={ onChangeHandler }
-        />
+        <input { ...componentProps } />
     );
 };
 
