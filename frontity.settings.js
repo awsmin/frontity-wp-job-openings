a5164cfadbadc8ac7b6c9de1767c652a5b2e776e
Anantajit JG <anantajitjg2010@gmail.com>
2020-07-08 13:04:35 +0530

    UPDATE: Convert to Frontity Project

---
+++ frontity.settings.js
@@ -0,0 +1,36 @@
+const settings = {
+  "name": "frontity-wp-job-openings",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "WP Job Openings",
+      "description": "Frontity package for WP Job Openings Plugin"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "https://demo.awsm.in/wp-job-openings/wp-json"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
