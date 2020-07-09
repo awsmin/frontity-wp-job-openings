1833932a08af683088037fea27d5d4dde36eda2b
Anantajit JG <anantajitjg2010@gmail.com>
2020-07-09 14:16:27 +0530

    UPDATE: Handle Multiple Checkboxes

--- packages/frontity-wp-job-openings/src/components/form/input.js
+++ packages/frontity-wp-job-openings/src/components/form/input.js
@@ -9,30 +9,50 @@ const Input = (props) => {
 
   const inputName = props.name;
   const inputValue = typeof props.value !== "undefined" ? props.value : "";
+  const isMultiCheckBox =
+    props.type === "checkbox" && inputName.indexOf("[]") !== -1;
 
   if (typeof state.awsmjobs.forms[jobId].values[inputName] === "undefined") {
-    actions.awsmjobs.setFieldValue(
-      jobId,
-      inputName,
-      props.type === "checkbox" ? "" : inputValue
-    );
+    let initialVal = inputValue;
+    if (props.type === "checkbox") {
+      initialVal = isMultiCheckBox ? [] : "";
+    }
+    actions.awsmjobs.setFieldValue(jobId, inputName, initialVal);
   }
+
+  let value = state.awsmjobs.forms[jobId].values[inputName];
 
   const onChangeHandler = (e) => {
     const target = e.target;
-    let currentValue = props.type === "file" ? target.files[0] : target.value;
+    let currentValue = "";
     if (props.type === "checkbox" || props.type === "radio") {
-      currentValue = target.checked ? currentValue : "";
+      if (isMultiCheckBox) {
+        let currentSet = new Set(value);
+        if (target.checked) {
+          currentSet.add(target.value);
+        } else {
+          currentSet.delete(target.value);
+        }
+        currentValue = [...currentSet];
+      } else {
+        currentValue = target.checked ? target.value : "";
+      }
+    } else if (props.type === "file") {
+      currentValue = target.files[0];
+    } else {
+      currentValue = target.value;
     }
     actions.awsmjobs.setFieldValue(jobId, inputName, currentValue);
   };
 
-  let value = state.awsmjobs.forms[jobId].values[inputName];
-
   if (props.type === "file") {
     props.ref = fileInput;
   } else if (props.type === "checkbox" || props.type === "radio") {
-    props.checked = inputValue === value ? true : false;
+    if (isMultiCheckBox) {
+      props.checked = value.includes(inputValue) ? true : false;
+    } else {
+      props.checked = inputValue === value ? true : false;
+    }
   } else {
     props.value = value;
   }
